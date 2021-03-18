203ac64d49b9d1feb58ea69ef1465994b63658c1
Johan Hansson <johan@anyfin.com>
2021-03-18 22:04:13 +0100

    UPDATE making the package executable from comandline

---
+++ README.md
@@ -0,0 +1,19 @@
+# pytesturl
+
+This libary scanes a directory for all urls and test if any of them is invalid
+
+### Local Installment
+
+pip install -e ''
+pip install -e ~/Documents/Johan/pytesturl
+
+## Links
+
+- https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+
+## TODO
+
+- Add ability to whitelist files
+- Add ability to whitelist urls
+- Paralize the job to speed it up
+- Add ability to pick if it shoudl throw a error os simply logg out
--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import regex as re
 import os
 import requests
 import argparse
+import yaml
+import logging
 files = []
 types = ['\.py', '\.md']
 cwd = os.getcwd()
 # TODO check a regex that is fine to steal?
-urls_regex = '(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
+urls_regex = """(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"""
+
+
+def read_configs(config_file: str):
+    configs = None
+    try:
+        if os.path.isfile(config_file):
+            with open('config_file', 'r') as f:
+                configs = yaml.load(f, Loader=yaml.FullLoader)
+        else:
+            logging.warning('congfig file could not be opend')
+    except:
+        logging.info('no config file is used')
+    return configs
 
 
 def extract_urls(file: str, rows: list, dirs: str) -> list:
@@ -23,11 +38,15 @@ def get_urls():
     for root, dirs, files in os.walk(cwd):
         for file in files:
             if re.search("|".join(types), file):
-                with open(root+'/'+file, 'r') as f:
-                    lines = f.readlines()
-                urls = extract_urls(file, lines, dirs)
-                if urls != []:
-                    output.append(urls[0])
+                # TODO fix this
+                try:
+                    with open(root+'/'+file, 'r') as f:
+                        lines = f.readlines()
+                    urls = extract_urls(file, lines, dirs)
+                    if urls != []:
+                        output.append(urls[0])
+                except:
+                    pass
     return output
 
 
@@ -37,28 +56,29 @@ def extract_404(urls):
         try:
             r = requests.get(url.get('url'))
             if r.status_code == 404:
-                errors[url.get('url'))]=url
+                errors[url.get('url')] = url
         except:
-            errors[url.get('url'))]=url
+            errors[url.get('url')] = url
 
     return errors
 
 
-def main(crash: bool=False):
-    urls=get_urls()
-    errors=extract_404(urls)
+def main(crash: bool = False, config_path: str = ''):
+    configs = read_configs(config_path)
+    urls = get_urls()
+    errors = extract_404(urls)
     if crash and len(errors) > 0:
         raise Exception("The directory contains broken links")
-    logging.warrning('Following errors was found in the directory')
-    logging.warrning(errors)
-
+    return errors
 
 
 if __name__ == "__main__":
 
-    arg_parser=argparse.ArgumentParser(
+    arg_parser = argparse.ArgumentParser(
         description='check all links in spec file types in the directory')
     arg_parser.add_argument('-crash', '--crash', required=False,
                             help='crash if any broken links are found')
-    args=vars(arg_parser.parse_args())
-    main(args['crash'])
+    arg_parser.add_argument('-config_path', '--config_path', required=False,
+                            help='path to config file')
+    args = vars(arg_parser.parse_args())
+    main(args['crash'], args['config_path'])
