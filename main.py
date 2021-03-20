06c96a9a270adda418bebdb00845450acf37c7da
NikeNano <niklas.sven.hansson@gmail.com>
2021-03-20 18:35:31 +0100

    clean up

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import yaml
 import logging
 import multiprocessing
 from multiprocessing import Pool
+from typing import List
+
 files = []
 types = ['\.py', '\.md']
 types = ['\.md']
@@ -16,46 +18,65 @@ CWD = os.getcwd()
 urls_regex = """(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"""
 
 
-def read_configs(config_file: str):
-    configs = None
-    try:
-        if os.path.isfile(config_file):
-            with open('config_file', 'r') as f:
-                configs = yaml.load(f, Loader=yaml.FullLoader)
-        else:
-            logging.warning('congfig file could not be opend')
-    except:
-        logging.info('no config file is used')
-    return configs
+def read_configs(config_file: str) -> dict:
+    """Function to parse the config file. 
+
+    Args:
+        config_file (str): File path to the config file. 
+
+    Returns:
+        dict: The configurations in the form of a dict
+    """
+    print(config_file)
+    if os.path.isfile(config_file):
+        with open(config_file, 'r') as f:
+            return  yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        logging.warning('config file could not be found, using defaults')
 
 
-def extract_urls(file: str, rows: list, dirs: str) -> list:
+def extract_urls(file: str, rows: list, file_path: str) -> List[str]:
+    """Function to find url:s in a file
+
+    Args:
+        file (str): The file name
+        rows (list): The number of lines in the file
+        file_path (str): The complete file path
+
+    Returns:
+        List[str]: List with all the URL:s in a file
+    """
     output = []
     for index, row in enumerate(rows):
         if re.search(urls_regex, row):
-            output.append({'url': re.search(urls_regex, row).group(
-                0), 'row': index, 'file': file, 'dirs': dirs})
+            output.append({
+                'url': re.search(urls_regex, row).group(0), 
+                'row': index, 
+                'file': file, 
+                'file_path': file_path})
     return output
 
 
-def get_urls():
+def get_urls() -> List[str]:
+    """Function to find
+
+    Returns:
+        List[str]: [description]
+    """
     output = []
     for root, dirs, files in os.walk(CWD):
         for file in files:
             if re.search("|".join(types), file):
-                # TODO fix this
-                try:
-                    with open(root+'/'+file, 'r') as f:
-                        lines = f.readlines()
-                    urls = extract_urls(file, lines, dirs)
-                    if urls != []:
-                        output.append(urls[0])
-                except:
-                    pass
+                file_path = root + '/' + file
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                urls = extract_urls(file, lines, file_path)
+                if len(urls) > 0:
+                    output.extend(urls)
     return output
 
 
-def extract_404(url):
+def extract_404(url: str):
     # TODO if it crahses we should take that as an error link
     errors = {}
     try:
@@ -68,36 +89,26 @@ def extract_404(url):
     return errors.get('output')
 
 
-def get_urls_extract_404(crash: bool, config_path: str):
-    configs = read_configs(config_path)
-    urls = get_urls()
-    return urls
-
-
-def main(crash: bool = False, directory: str = None, config_path: str = '',):
+def main(crash: bool, directory: str, config_path: str):
     if directory:
         CWD = directory
-
+    #[TODO] fix use configs or defaults for all things
+    # replace the had code types for example. 
+    #configs = read_configs(config_path)
     num_cores = multiprocessing.cpu_count()
-    print(f"num_cores={num_cores}")
+    print(f"nbr of cores={num_cores}")
     pool = Pool(num_cores)
-    errors = pool.map(extract_404, get_urls_extract_404(crash, config_path))
+    # Currently we are first getting all the URL:s
+    # Then we are testing them, 
+    # I think that the testing is super fast but finding the stuff is not
+    # I think we should move around the parallism to a lower layer instead. 
+    # list_of_things = []
+    # Get all the links then run a function for each of them
+    # This will make it work fine. 
+
+    errors = pool.map(extract_404, get_urls())
     errors = [error for error in errors if error != None]
-    if crash and len(errors) > 0:
-        raise Exception("The directory contains broken links")
     for error in errors:
         logging.warning(error)
     if crash and errors:
         raise Exception('There are broken urls in the directory')
-
-
-if __name__ == "__main__":
-
-    arg_parser = argparse.ArgumentParser(
-        description='check all links in spec file types in the directory')
-    arg_parser.add_argument('-crash', '--crash', required=False,
-                            help='crash if any broken links are found')
-    arg_parser.add_argument('-config_path', '--config_path', required=False,
-                            help='path to config file')
-    args = vars(arg_parser.parse_args())
-    main(args['crash'], args['config_path'])
