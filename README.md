ae5901b2dc1848b6e56364ba20dff68ace183fdf
Johan Hansson <johan@anyfin.com>
2021-03-22 21:44:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# url-test
+
+This library scan a directory for all urls and test if any of them is invalid
+
+### Local Installment
+
+pip install -e ''
+pip install -e ~/Documents/Johan/url-test
+
+## Links
+
+- https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+
+## TODO
+
+- Add ability to whitelist files
+- Add ability to whitelist urls
+- Parallelize the job to speed it up
+- Add ability to pick if it should throw a error os simply log out
+- Clean up the history of the branch so it start from empty!
+- Rename
+
+## Broken test link
+
+https://github.com/FortAwesome/Font-Awesome-Pro/issues
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="url-test",
+    version="0.0.2",
+    author="Johan Hansson",
+    author_email="johan.eric.hansson@gmail.com",
+    description="This package check urls in the directory and sub directories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JohanWork/url-test",
+    scripts=['url-test'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['pyyaml']
+)
